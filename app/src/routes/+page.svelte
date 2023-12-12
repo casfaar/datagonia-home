87c11f1ce67f56332ddc9d8421e6656b9102bb6d
Casper Faarlund <caspef@ntnu.no>
2023-12-12 13:02:55 +0100

    Add background and logo

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,12 +2,68 @@
   //This is a page that shows an image as the background, and has a pomodoro timer in the middle, styled with a transparent glass dark mode theme with nice fonts.
   //The pomodoro is 25min work and 5min break, and it has a 15min break every 4 pomodoros.
 
-
+import patamountains from '$lib/patamountains.svg';
 </script>
+<body>
+  <div class="svg-container">
+    <img src={patamountains} alt="patamountains" />
+  </div>
+</body>
 
-Homepage for datagonia
 
 <style>
   
+  body {
+            background-color: black;
+            margin: 0;
+            padding: 0;
+        }
+        
+        .svg-container {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            z-index: -1;
+        }
+        img {
+            width: 100%;
+            height: auto;
+        }
+        
+        .content {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
 
+            margin-top: 30%;
+        }
+        
+        .rows {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+        }
+        .button {
+            background-color: #ffffff;
+            color: #000000;
+            padding: 10px 20px;
+            margin: 0 10px;
+            border-radius: 5px;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        
+        .created-by {
+            position: fixed;
+            bottom: 10px;
+            left: 50%;
+            transform: translateX(-50%);
+            color: #ffffff;
+        }
+        p {
+            color: #ffffff;
+        }
 </style>
