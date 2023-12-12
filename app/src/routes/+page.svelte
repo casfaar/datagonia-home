5e2db4f30b648170ec6d2f04a6e3ba25dcb41f83
Casper Faarlund <caspef@ntnu.no>
2023-12-12 13:36:10 +0100

    remove card import

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Card from '$lib/Card.svelte';
+
   //This is a page that shows an image as the background, and has a pomodoro timer in the middle, styled with a transparent glass dark mode theme with nice fonts.
   //The pomodoro is 25min work and 5min break, and it has a 15min break every 4 pomodoros.
 
